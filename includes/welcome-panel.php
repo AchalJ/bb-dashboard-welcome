73e29319fe97486029c7962f992fd455d808170e
Achal Jain <achal@ideaboxcreations.com>
2016-12-01 13:06:38 +0530

    Added JS snippet to load font-awesome CSS conditionally

--- includes/welcome-panel.php
+++ includes/welcome-panel.php
@@ -10,3 +10,11 @@
 <?php
     echo do_shortcode('[fl_builder_insert_layout slug="'.self::$template[self::$current_role].'"]');
 ?>
+
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+        if ( $('.fl-builder-content').find('.fa').length > 0 ) {
+            $('.fl-builder-content').prepend('<link rel="stylesheet" id="font-awesome-css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" media="all">');
+        }
+    });
+</script>
